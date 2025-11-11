f382674c3ca8a4abc281103c77f0551a2e8c9d10
SeleneFleita <fleitaselene26@gmail.com>
2025-11-10 21:50:41 -0300

    feat: :bug: Configuracion de cors con fast api para frontend

--- client/main.py
+++ client/main.py
@@ -1,10 +1,27 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from routers.modelRouter import modelRouter
 
 app = FastAPI(title="Gestor de Competencia - Formosa", version="0.1.0")
 
+# 🌐 Configuración de CORS
+origins = [
+    "http://localhost:5173",   # frontend local (Vite)
+    "http://127.0.0.1:5173",
+    # podés agregar más dominios si desplegás el frontend
+    # "https://mercalytica.vercel.app",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,      # dominios permitidos
+    allow_credentials=True,
+    allow_methods=["*"],        # permite todos los métodos (GET, POST, etc.)
+    allow_headers=["*"],        # permite todos los encabezados
+)
+
 app.include_router(modelRouter)
 
 if __name__ == "__main__":
-     uvicorn.run("main:app", port=6000,reload=True)
+     uvicorn.run("main:app", port=5001,reload=True)
